763a4485217f3f8b30d8cacb6d9bf404a37b00c4
Ryan Cotter <ryancotter000@gmail.com>
2023-07-20 08:08:11 -0600

    Changes to be committed: 	node setup, D3 modules
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   rendering/graph.js
    	new file:   rendering/miserables.json
    	new file:   scraper/browser.js
    	new file:   scraper/index.js
    	new file:   scraper/package-lock.json
    	new file:   scraper/package.json
    	new file:   scraper/pageController.js
    	new file:   scraper/pageScraper.js
    	new file:   seeds and tags/crawler seeds.txt
    	new file:   seeds and tags/math hashtags.txt
    	new file:   tagger/tagger.ts
    	new file:   wikiWorld.js

---
+++ scraper/pageScraper.js
@@ -0,0 +1,23 @@
+const scraperObject = {
+	url: 'https://en.wikipedia.org/wiki/Computer',
+	async scraper(browser){
+		let page = await browser.newPage();
+		console.log(`Navigating to ${this.url}...`);
+		await page.goto(this.url);
+        // Wait for the required DOM to be rendered
+		await page.waitForSelector('.mw-page-container');
+		// Get the link to all the required books
+
+
+		let urls = [];
+		let links = document.querySelectorAll('p > a');
+
+		for (link in links){
+			urls.push(link).href 
+		}
+		console.log(urls);
+		
+	}
+};
+
+module.exports = scraperObject;
---
+++ wikiWorld.js
@@ -0,0 +1,8 @@
+var pageCount = 0;
+
+function crawlMain(link1);
+
+while (pageCount <1000){
+    crawlMain();
+    pageCount++;
+};
